cafcd2056f622e3727cedb6679154c4759a0c984
quangnhat09z <160835073+quangnhat09z@users.noreply.github.com>
2024-04-21 20:28:51 +0700

    Create Bai6.cpp

---
+++ LTNC-05/Bai6.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int type, marks;
+    string name;
+    
+    vector<int> result;
+    map<string, int> mp;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> type;
+        if (type == 1) {
+            cin >> name >> marks;
+            mp[name] += marks;
+        } else if (type == 2) {
+            cin >> name;
+            mp.erase(name);
+        } else if (type == 3) {
+            cin >> name;
+            result.push_back(mp[name]);
+        }
+    }
+    
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << endl;
+    }
+    return 0;
+}
